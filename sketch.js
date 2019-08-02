4cfdf299e3232feee9c18dfed35222e95710b819
saniyaeram <53005024+saniyaeram@users.noreply.github.com>
2019-08-02 20:45:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,27 @@
+var fixedRect, movingRect;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,800);
+  fixedRect = createSprite(200,200,50,80);
+  fixedRect.shapeColor = "yellow";
+  movingRect=createSprite(400,200,80,30);
+  movingRect.shapeColor = "yellow";
 }
 
 function draw() {
-  background(255,255,255);  
+  background(50,100,150);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;  
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2){
+
+    movingRect.shapeColor = "pink";
+    fixedRect.shapeColor = "pink";
+
+  }
+  else{
+    movingRect.shapeColor = "yellow";
+    fixedRect.shapeColor = "yellow";
+  }
   drawSprites();
 }
